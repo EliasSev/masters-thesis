c20070744c92e812767ab73992b8849fdc41440c
Elias J Severinsen <elias.j.severinsen@gmail.com>
2026-01-21 16:52:39 +0100

    Mathematical Background: add 2 new sections

---
+++ notebooks/colors.py
@@ -0,0 +1,24 @@
+"""
+Colors and color maps used for the Thesis.
+"""
+
+class C:
+    # IBM color palette
+    c5 = [
+        "#648FFF",
+        "#785EF0",
+        "#DC267F",
+        "#FE6100",
+        "#FFB000"
+    ]
+    c3 = [
+        "#648FFF",
+        "#785EF0",
+        "#DC267F",
+    ]
+    c2 = [
+        "#648FFF",
+        "#DC267F"
+    ]
+    # Color map
+    cmap = 'plasma'
